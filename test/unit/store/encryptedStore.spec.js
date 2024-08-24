3ace3fa2f31692bce0cfd57a3b0260cf164d3d50
David Carboni <david@carboni.io>
2024-08-24 19:42:50 +0100

    Take out underscores

--- test/unit/store/encryptedStore.spec.js
+++ test/unit/store/encryptedStore.spec.js
@@ -9,31 +9,31 @@ var Store = require('../../../lib/store/Store');
 var EncryptedStore = require('../../../lib/store/EncryptedStore');
 var FileSessionHandler = require('../../../lib/handler/FileSessionHandler');
 
-describe('EncryptedStore', function(){
-    var sessionStoragePath ='./test/sessions';
-    var value = "data to store";
-    var handler =  new FileSessionHandler(sessionStoragePath);
-    var encryptedStore = new EncryptedStore('node_session', handler, null, 'sdhfjasdfasjdhfjhsajdhfjhasdfsdksdf');
+describe('EncryptedStore', function () {
+  var sessionStoragePath = './test/sessions';
+  var value = "data to store";
+  var handler = new FileSessionHandler(sessionStoragePath);
+  var encryptedStore = new EncryptedStore('node_session', handler, null, 'sdhfjasdfasjdhfjhsajdhfjhasdfsdksdf');
 
-    describe('#constructor', function(){
-        it('Should be an instance of EncryptedStore and Store', function(done){
-            encryptedStore.should.be.an.instanceOf(EncryptedStore);
-            encryptedStore.should.be.an.instanceOf(Store);
-            done();
-        });
+  describe('#constructor', function () {
+    it('Should be an instance of EncryptedStore and Store', function (done) {
+      encryptedStore.should.be.an.instanceOf(EncryptedStore);
+      encryptedStore.should.be.an.instanceOf(Store);
+      done();
     });
+  });
 
-    describe('method#__prepareForStorage', function(){
-        it('should encrypt and return given data', function(done){
-            encryptedStore.__prepareForStorage(value).should.not.eql(value);
-            done();
-        })
-    });
-
-    describe('method#__prepareForParse', function(){
-        it('should decrypt and return given data', function(done){
-            encryptedStore.__prepareForParse(encryptedStore.__prepareForStorage(value)).should.eql(value);
-            done();
-        })
+  describe('method#prepareForStorage', function () {
+    it('should encrypt and return given data', function (done) {
+      encryptedStore.prepareForStorage(value).should.not.eql(value);
+      done();
     })
+  });
+
+  describe('method#prepareForParse', function () {
+    it('should decrypt and return given data', function (done) {
+      encryptedStore.prepareForParse(encryptedStore.prepareForStorage(value)).should.eql(value);
+      done();
+    })
+  })
 });
