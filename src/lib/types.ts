6e20d5a4e498214e0f16daf1a9d536b1a675fd13
David Carboni <david@carboni.io>
2024-08-24 19:43:54 +0100

    Add SessionHandler and improve Config

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -10,11 +10,19 @@ export interface Config {
   lottery: number[];
   cookie: string;
   path: string,
-  domain: string | null,
+  domain?: string,
   secure: boolean,
   httpOnly: boolean,
   encrypt: boolean,
   secret: string,
   trustProxy: boolean,
-  trustProxyFn: Function | null,
+  trustProxyFn?: Function,
+}
+
+export interface SessionHandler {
+  read: (sessionId: string, callback: (session: any) => void) => void,
+  write: (sessionId: string, data: string, callback: (err?: Error) => void) => void,
+  destroy: (sessionId: string, callback: (err?: Error) => void) => void,
+  gc: (maxAge: string | number) => void,
+  setExists: <T extends SessionHandler>(value: boolean) => T,
 }
