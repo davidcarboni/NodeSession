a12b552942bfe76c69845ac0c097b0ad52e7d4f9
David Carboni <david@carboni.io>
2024-08-24 18:21:24 +0100

    Draft conversion

--- src/lib/handler/DatabaseSessionHandler.ts
+++ src/lib/handler/DatabaseSessionHandler.ts
@@ -5,8 +5,10 @@
  * @copyright 2015, Harish Anchu. All rights reserved.
  * @license Licensed under MIT
  */
-
-function DatabaseSessionHandler(model) {
+export class DatabaseSessionHandler {
+  private __model: any;
+  private __exists: boolean;
+  constructor(model) {
     /**
      * Session table waterline model instance.
      *
@@ -21,110 +23,100 @@ function DatabaseSessionHandler(model) {
      * @type {Boolean}
      */
     this.__exists;
-}
+  }
 
-/**
- * Reads the session data.
- *
- * @param {String} sessionId
- * @param {function} callback
- */
-DatabaseSessionHandler.prototype.read = function (sessionId, callback) {
+  /**
+   * Reads the session data.
+   */
+  read(sessionId: string, callback: Function) {
     var self = this;
     var response = '';
 
     this.__model.findOne(sessionId, function (err, session) {
-        if (!err && session && session.payload) {
-            self.__exists = true;
+      if (!err && session && session.payload) {
+        self.__exists = true;
 
-            response = JSON.parse(session.payload);
-        }
+        response = JSON.parse(session.payload);
+      }
 
-        if (callback) {
-            callback(response);
-        }
+      if (callback) {
+        callback(response);
+      }
     });
-};
+  };
 
-/**
- * Writes the session data to the storage.
- *
- * @param {String} sessionId
- * @param {String} data
- * @param {function} callback
- */
-DatabaseSessionHandler.prototype.write = function (sessionId, data, callback) {
+  /**
+   * Writes the session data to the storage.
+   */
+  write(sessionId: string, data: string, callback: Function) {
     var self = this;
     if (this.__exists) {
-        this.__model.update(sessionId,
-            {payload: JSON.stringify(data), lastActivity: (new Date()).getTime()},
-            function (err, record) {
-                if (!err) {
-                    self.__exists = true;
-                }
+      this.__model.update(sessionId,
+        { payload: JSON.stringify(data), lastActivity: (new Date()).getTime() },
+        function (err: Error, record: any) {
+          if (!err) {
+            self.__exists = true;
+          }
 
-                if (callback) {
-                    callback(err);
-                }
-            }
-        );
+          if (callback) {
+            callback(err);
+          }
+        }
+      );
     }
     else {
-        this.__model.create(
-            {
-                id: sessionId,
-                payload: JSON.stringify(data),
-                lastActivity: (new Date()).getTime()
-            },
-            function (err, record) {
-                if (!err) {
-                    self.__exists = true;
-                }
+      this.__model.create(
+        {
+          id: sessionId,
+          payload: JSON.stringify(data),
+          lastActivity: (new Date()).getTime()
+        },
+        function (err: Error, record: any) {
+          if (!err) {
+            self.__exists = true;
+          }
 
-                if (callback) {
-                    callback(err);
-                }
-            }
-        );
+          if (callback) {
+            callback(err);
+          }
+        }
+      );
     }
 
-};
+  };
 
-/**
- * Destroys a session.
- *
- * @param {String} sessionId
- * @param {function} callback
- */
-DatabaseSessionHandler.prototype.destroy = function (sessionId, callback) {
+  /**
+   * Destroys a session.
+   */
+  destroy(sessionId: string, callback: Function) {
     this.__model.destroy(sessionId, function (err) {
-        if (callback) {
-            callback(err);
-        }
-    })
-};
-
-/**
- * Cleans up expired sessions (garbage collection).
- *
- * @param {String|number} maxAge Sessions that have not updated for the last maxAge seconds will be removed
- */
-DatabaseSessionHandler.prototype.gc = function (maxAge) {
-    var age = (new Date()).getTime() - maxAge;
-    this.__model.destroy({lastActivity: {'<': age}}, function (err) {
+      if (callback) {
+        callback(err);
+      }
     });
-};
+  };
 
-/**
- * Set the existence state for the session.
- *
- * @param  {Boolean} value
- * @return {DatabaseSessionHandler}
- */
-DatabaseSessionHandler.prototype.setExists = function (value) {
+  /**
+   * Cleans up expired sessions (garbage collection).
+   *
+   * @param maxAge Sessions that have not updated for the last maxAge seconds will be removed
+   */
+  gc(maxAge: string | number) {
+    var age = (new Date()).getTime() - +(maxAge);
+    this.__model.destroy({ lastActivity: { '<': age } }, function (err) {
+    });
+  };
+
+  /**
+   * Set the existence state for the session.
+   *
+   * @param  {Boolean} value
+   * @return {DatabaseSessionHandler}
+   */
+  setExists(value: boolean): DatabaseSessionHandler {
     this.__exists = value;
 
     return this;
-};
+  };
 
-module.exports = DatabaseSessionHandler;
+}
