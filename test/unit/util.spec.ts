3392a5486be3c9bb845e74c3e2eb175db2c9858f
David Carboni <david@carboni.io>
2024-08-24 22:17:23 +0100

    Trying to get tests working

--- test/unit/util.spec.ts
+++ test/unit/util.spec.ts
@@ -6,17 +6,17 @@
  * @license Licensed under MIT
  */
 
-import util from '../../util';
+import util from '../../src/util';
 
 describe('util', function () {
   describe('method#defineMember', function () {
     it('should define up to a parent member on given object by dot notation', function (done) {
-      var obj = {};
+      var obj: any = {};
 
       util.defineMember(obj, 'a.b.c.d');
 
       obj.a.b.c.should.be.ok;
       done();
-    })
+    });
   });
 });
