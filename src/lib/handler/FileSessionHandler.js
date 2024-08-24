e297a8e02d7888f82f1353338c06672addb4b6e8
David Carboni <david@carboni.io>
2024-08-24 18:14:17 +0100

    Partially pre-converted

--- src/lib/handler/FileSessionHandler.js
+++ src/lib/handler/FileSessionHandler.js
@@ -9,7 +9,8 @@
 var fs = require('fs-extra');
 var path = require('path');
 
-function FileSessionHandler(path) {
+export class FileSessionHandler {
+  constructor(path) {
     /**
      * The path where sessions should be stored.
      *
@@ -21,78 +22,78 @@ function FileSessionHandler(path) {
      * Create directory for session storage.
      */
     fs.mkdirsSync(this.__path);
-}
+  }
 
-/**
- * Reads the session data.
- *
- * @param {String} sessionId
- * @param {function} callback
- */
-FileSessionHandler.prototype.read = function (sessionId, callback) {
+  /**
+   * Reads the session data.
+   *
+   * @param {String} sessionId
+   * @param {function} callback
+   */
+  read(sessionId, callback) {
     fs.readFile(path.join(this.__path, sessionId), 'utf-8', function (err, file) {
-        if (err) {
-            file = '';
-        }
-        if (callback) {
-            callback(file);
-        }
+      if (err) {
+        file = '';
+      }
+      if (callback) {
+        callback(file);
+      }
     });
-};
+  };
 
-/**
- * Writes the session data to the storage.
- *
- * @param {String} sessionId
- * @param {String} data
- * @param {function} callback
- */
-FileSessionHandler.prototype.write = function (sessionId, data, callback) {
+  /**
+   * Writes the session data to the storage.
+   *
+   * @param {String} sessionId
+   * @param {String} data
+   * @param {function} callback
+   */
+  write(sessionId, data, callback) {
     fs.writeFile(path.join(this.__path, sessionId), data, 'utf-8', function (err) {
-        if (callback) {
-            callback(err);
-        }
+      if (callback) {
+        callback(err);
+      }
     });
-};
+  };
 
-/**
- * Destroys a session.
- *
- * @param {String} sessionId
- * @param {function} callback
- */
-FileSessionHandler.prototype.destroy = function (sessionId, callback) {
+  /**
+   * Destroys a session.
+   *
+   * @param {String} sessionId
+   * @param {function} callback
+   */
+  destroy(sessionId, callback) {
     fs.unlink(path.join(this.__path, sessionId), function (err) {
-        if (callback) {
-            callback(err)
-        }
+      if (callback) {
+        callback(err)
+      }
     })
-};
+  };
 
-/**
- * Cleans up expired sessions (garbage collection).
- *
- * @param {String|number} maxAge Sessions that have not updated for the last maxAge seconds will be removed
- */
-FileSessionHandler.prototype.gc = function (maxAge) {
+  /**
+   * Cleans up expired sessions (garbage collection).
+   *
+   * @param {String|number} maxAge Sessions that have not updated for the last maxAge seconds will be removed
+   */
+  gc(maxAge) {
     var self = this;
 
     fs.readdir(self.__path, function (err, files) {
-        if (err || files.length === 0) {
-            return;
-        }
-        files.forEach(function (file) {
-            if (file[0] != '.') {
-                fs.stat(path.join(self.__path, file), function (err, stat) {
-                    if (!err) {
-                        if (stat.isFile() && ((new Date()).getTime() - stat.atime.getTime() > maxAge)) {
-                            self.destroy(file);
-                        }
-                    }
-                });
+      if (err || files.length === 0) {
+        return;
+      }
+      files.forEach(function (file) {
+        if (file[0] != '.') {
+          fs.stat(path.join(self.__path, file), function (err, stat) {
+            if (!err) {
+              if (stat.isFile() && ((new Date()).getTime() - stat.atime.getTime() > maxAge)) {
+                self.destroy(file);
+              }
             }
-        });
+          });
+        }
+      });
     });
-};
+  };
 
-module.exports = FileSessionHandler;
+}
