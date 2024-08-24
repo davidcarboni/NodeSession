f1bab047a9af8f9a11bf2ec6c747e140b91257ef
David Carboni <david@carboni.io>
2024-08-24 18:23:01 +0100

    Pre-convert

--- src/lib/util.js
+++ src/lib/util.js
@@ -8,53 +8,59 @@
 
 var proxyaddr = require('proxy-addr');
 
-var util = {};
+var util = {
 
-/**
- * Define a object key by dot notation
- *
- * @param {Object} obj
- * @param {String} name
- */
-util.defineMember = function (obj, name) {
+
+  /**
+   * Define a object key by dot notation
+   *
+   * @param {Object} obj
+   * @param {String} name
+   */
+  defineMember: function (obj, name) {
     var nameSplit = name.split('.');
     var i;
     var exists = obj;
 
     for (i = 0; i < nameSplit.length - 1; i++) {
-        if (!exists.hasOwnProperty(nameSplit[i]) || typeof exists[nameSplit[i]] !== 'object') {
-            exists[nameSplit[i]] = {};
-        }
-        exists = exists[nameSplit[i]]
+      if (!exists.hasOwnProperty(nameSplit[i]) || typeof exists[nameSplit[i]] !== 'object') {
+        exists[nameSplit[i]] = {};
+      }
+      exists = exists[nameSplit[i]]
     }
-};
+  },
 
-/**
- * Compile "proxy trust" value to function.
- *
- * @param  {Boolean|String|Number|Array|Function} val
- * @return {Function}
- */
+  /**
+   * Compile "proxy trust" value to function.
+   *
+   * @param  {Boolean|String|Number|Array|Function} val
+   * @return {Function}
+   */
 
-util.compileTrust = function(val) {
+  compileTrust: function (val) {
     if (typeof val === 'function') return val;
 
     if (val === true) {
-        // Support plain true/false
-        return function(){ return true };
+      // Support plain true/false
+      return function () { return true };
     }
 
     if (typeof val === 'number') {
-        // Support trusting hop count
-        return function(a, i){ return i < val };
+      // Support trusting hop count
+      return function (a, i) { return i < val };
     }
 
     if (typeof val === 'string') {
-        // Support comma-separated values
-        val = val.split(/ *, */);
+      // Support comma-separated values
+      val = val.split(/ *, */);
     }
 
     return proxyaddr.compile(val || []);
+  },
+
+
+
 };
+
 
 module.exports = util;
